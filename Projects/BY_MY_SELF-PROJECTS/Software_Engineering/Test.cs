31cbca2b1900210adbae30d6c574c5de0d596fb6
Blessed <bleemmjochi@gmail.com>
2026-09-20 16:53:56 +0100

    Prepared my BY YOUR SELF PROJECT WORKING SPACE

---
+++ Projects/BY_MY_SELF-PROJECTS/Software_Engineering/Test.cs
@@ -0,0 +1,14 @@
+
+partial class Program
+{
+	static void Main()
+	{
+		int speed = "100";
+		int fuel = 50;
+		bool engine = true;
+
+		Console.WriteLine("Speed: " + speed);
+		Console.WriteLine("Fuel: " + fuel);
+		Console.WriteLine("Engine: " + engine);
+	}
+}
